2f4e4c17235e7a489f5913a541a090b21c799a41
sainathdhatti <sainathd@valueaddsofttech.com>
2024-08-07 17:36:51 +0530

    added cloudinary and food itms module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,48 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MulterModule } from '@nestjs/platform-express';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { diskStorage } from 'multer';
+import * as path from 'path';
+import { FoodEntity } from './Food_module/Food_items/food_itm.entity';
+import { Food_itemsModule } from './Food_module/Food_items/food_itm.module';
+import { CloudinaryModule } from './cloudinary/cloudinary.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USER'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        entities: [FoodEntity],
+        synchronize: true,
+      }),
+    }),
+    TypeOrmModule.forFeature([FoodEntity]),
+    MulterModule.register({
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, cb) => {
+          const filename: string = path.parse(file.originalname).name.replace(/\s/g, '') + path.extname(file.originalname);
+          cb(null, `${filename}`);
+        },
+      }),
+    }),
+    Food_itemsModule,
+    CloudinaryModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
